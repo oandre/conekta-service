327032b18a7115986fc79e3811eae7bbd2b300ad
André Simões <sou@oand.re>
2014-05-21 16:24:45 -0300

    Changes to make a simple request

--- TestAbstraction.php
+++ TestAbstraction.php
@@ -12,7 +12,22 @@ $charge->setAmount('20000');
 $charge->setCurrency('MXN');
 $charge->setReference(uniqid('debit_'));
 $charge->setToken('tok_test_visa_4242');
-$charge->setBasicParameters();
 
 $service = new Service();
-$service->createCharge($charge);
+$response = $service->createCharge($charge);
+
+//$chargeFind = new ChargeRequest();
+//$chargeFind->setId($response->getId());
+//
+//$responseFind = $service->findCharge($chargeFind);
+//
+//$chargeRefund = new ChargeRequest();
+//$chargeRefund->setId($response->getId());
+//
+//$responseRefund = $service->refundCharge($chargeRefund);
+
+$chargeCapture = new ChargeRequest();
+$chargeCapture->setId($response->getId());
+
+$responseCapture = $service->refundCharge($chargeCapture);
+
